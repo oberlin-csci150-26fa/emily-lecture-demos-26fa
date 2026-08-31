358db5490ac431436a3dbf55ca388a0dc3292fdd
Emily Q. Wang <greenteawarrior@users.noreply.github.com>
2026-08-31 22:09:20 +0000

    temperature conversion demo

---
+++ temperature.py
@@ -0,0 +1,24 @@
+# Goal: Convert Fahrenheit to Celsius
+# 32F --> 0C
+# 41F --> 5C
+# -40F --> -40C
+
+# Inputs
+temp_f = -40
+
+# Work
+temp_c = (temp_f - 32) * 5/9
+
+# Outputs
+print(temp_c)
+
+# Goal: Convert Celsius to Fahrenheit
+
+# Inputs
+temp_c = 0
+
+# Work
+temp_f = temp_c/5*9+32
+
+# Outputs
+print(temp_f)
