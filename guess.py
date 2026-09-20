ffc5d776a7cb996a312204f18e0f6b06b10086ea
Emily Q. Wang <ewang1@oberlin.edu>
2026-09-19 21:10:10 -0400

    guess.py completed coding demo

--- guess.py
+++ guess.py
@@ -4,7 +4,16 @@ won = False # variable for game ongoing / game over
 goal = randint(0, 9)
 
 while not won: # run indefinitely until game over
-  guess = int(input("Guess a number between 1-9:"))
+  guess = int(input("Guess a number between 0-9:"))
   if guess == goal:
     print("You win!")
     # clicker question: what goes here?
+    won = True
+
+# Suggestion: Give a hint about whether you went over or under. (hot...warm...tepid...chill...cold)
+  # Extend the if statement for when guess < goal, guess > goal, etc.
+# Suggestion: Three strikes and "You lose!"
+  # Keep track of while loop runs, increase a variable.
+# Suggestion: User selects the range instead of it being "hard-coded."
+# Suggestion: Lose condition number. 
+# Suggestion: Give a hint about whether you went close to the evil number.
